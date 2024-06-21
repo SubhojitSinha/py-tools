fe167e10cfcdaf90a36b24ba5336b5593ee4d06b
Subhojit Sinha <51361776+SubhojitSinha@users.noreply.github.com>
2024-06-21 15:19:57 +0530

    Update print-folder-structure.py with file and folder icons

--- print-folder-structure.py
+++ print-folder-structure.py
@@ -3,7 +3,6 @@ Module that generates and prints a tree representation of the directory structur
 """
 
 import os
-
 def generate_tree(path, gitignore_patterns):
     """
     Generates a tree representation of the directory structure starting from the given `path`.
@@ -57,9 +56,38 @@ def print_tree(tree, prefix=''):
         if loop == total and subtree is not None:
             handle1 = p1
             handle2 = '    '
-        print(f"{prefix}{handle1}{item}{lineend}")
+
+        icon = "🗂️  " if subtree is not None else "📄 "
+        print(f"{prefix}{handle1}{icon}{item}{lineend}")
         if subtree is not None:
             print_tree(subtree, prefix + handle2)
+            # print(p3)
+
+# def sort_dict(d):
+#     """ Recursively sort a dictionary and its sub-dictionaries by keys. """
+#     if not isinstance(d, dict):
+#         return d
+
+#     # Recursively sort each sub-dictionary
+#     return {k: sort_dict(v) for k, v in sorted(d.items())}
+
+
+def sort_key(item):
+    key, value = item
+    if value is None:
+        return (0, key)  # Group None values first
+    elif isinstance(value, dict):
+        return (2, key)  # Group sub-dictionaries last
+    else:
+        return (1, key)  # Group other items in the middle
+
+def sort_dict(d):
+    """ Recursively sort a dictionary by custom rules and sort sub-dictionaries. """
+    if not isinstance(d, dict):
+        return d
+
+    sorted_items = sorted(d.items(), key=sort_key)
+    return {k: sort_dict(v) for k, v in sorted_items}
 
 def main():
     """
@@ -82,7 +110,18 @@ def main():
 
     gitignore_patterns.append('.git')
     tree = generate_tree(current_dir, gitignore_patterns)
+
+    # Remove this file from the tree before printing
+    this_file = os.path.basename(__file__)
+    del tree[this_file]
+
+    # sort the tree
+    tree = sort_dict(tree)
+
+    # print the tree
     print_tree(tree)
+
+
 
 if __name__ == "__main__":
     main()
